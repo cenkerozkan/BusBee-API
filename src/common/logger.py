476c8b883c20133e905c062c2111320982553d25
cenkerozkan <ozkan.cenker@student.atilim.edu.tr>
2025-03-09 00:08:56 +0300

    Update logger formatter for improved log message clarity

--- src/common/logger.py
+++ src/common/logger.py
@@ -3,7 +3,7 @@ import logging
 def get_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(filename)s:%(lineno)d - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(levelname)s - %(asctime)s - %(filename)s:%(lineno)d - %(name)s - %(message)s')
     ch = logging.StreamHandler()
     ch.setFormatter(formatter)
     logger.addHandler(ch)
