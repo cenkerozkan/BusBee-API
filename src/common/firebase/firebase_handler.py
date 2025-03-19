76ceb21dd886bd44163e25f5493748a556010381
cenkerozkan <ozkan.cenker@student.atilim.edu.tr>
2025-03-19 19:23:58 +0300

    A mini refactoring for solving the issue of firebase could not parse the private key taken from the .env

--- src/common/firebase/firebase_handler.py
+++ src/common/firebase/firebase_handler.py
@@ -24,7 +24,7 @@ class FirebaseHandler(metaclass=SingletonMeta):
                 "type": os.getenv("TYPE"),
                 "project_id": os.getenv("PROJECT_ID"),
                 "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-                "private_key": os.getenv("PRIVATE_KEY"),
+                "private_key": os.getenv("PRIVATE_KEY").replace(r'\n', '\n'),
                 "client_email": os.getenv("CLIENT_EMAIL"),
                 "client_id": os.getenv("CLIENT_ID"),
                 "auth_uri": os.getenv("AUTH_URI"),
